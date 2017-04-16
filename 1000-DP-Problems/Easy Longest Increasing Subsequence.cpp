c69409a0e4137716c85b0b4c068558f0725445ee
Prajeeth <Prajeeth@Prajeeth-PC>
2017-04-16 23:22:58 +0530

    First solved.

--- 1000-DP-Problems/Easy Longest Increasing Subsequence.cpp
+++ 1000-DP-Problems/Easy Longest Increasing Subsequence.cpp
@@ -14,6 +14,21 @@ int main() {
 #ifdef HOME
 	freopen("in", "r", stdin);
 #endif
+	int n; cin >> n;
+	vi v;
+	rep(i, n) {
+		int a; cin >> a;
+		v.push_back(a);
+	}
+	vi dp(n, 1);
+	for (int i=1; i<n; i++) {
+		for (int j=i-1; j >=0; j--) {
+			if (v[j] < v[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
 
+	cout << *max_element(all(dp));
 	return 0;
 }
