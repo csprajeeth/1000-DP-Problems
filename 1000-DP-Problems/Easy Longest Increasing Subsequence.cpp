29afe7f35436b631c62a7c1e76c359bf2e77fcf1
Prajeeth <Prajeeth@Prajeeth-PC>
2017-04-16 22:07:11 +0530

    First test commit.

---
+++ 1000-DP-Problems/Easy Longest Increasing Subsequence.cpp
@@ -0,0 +1,19 @@
+// http://www.spoj.com/problems/ELIS/
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<bool> vb;
+typedef pair<int, int> pii;
+
+#define rep(i, n) for(int i=0; i<(int)n; i++)
+#define all(X) (X).begin(),(X).end()
+
+int main() {
+#ifdef HOME
+	freopen("in", "r", stdin);
+#endif
+
+	return 0;
+}
